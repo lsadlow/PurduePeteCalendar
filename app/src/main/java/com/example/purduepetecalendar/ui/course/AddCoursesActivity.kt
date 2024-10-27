c61c1b42cfb77426ba8a2b845c8f404ebcdd131a
JasonX354 <xjason354@gmail.com>
2024-10-26 20:38:32 -0400

    Change List<> items to MutableList<> items. Made createCourse() and addItem() functions but am unsure if they work

--- app/src/main/java/com/example/purduepetecalendar/ui/course/AddCoursesActivity.kt
+++ app/src/main/java/com/example/purduepetecalendar/ui/course/AddCoursesActivity.kt
@@ -2,21 +2,47 @@ package com.example.purduepetecalendar.ui.course
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.purduepetecalendar.R
+import java.time.DayOfWeek
 import java.time.LocalTime
 
 class AddCoursesActivity : AppCompatActivity() {
+
     private lateinit var layoutInflater: LayoutInflater
+    lateinit var courseName: String
+
+    var items: MutableList<Course> = mutableListOf()
+
+    fun createCourse(
+        name: String,
+        days: List<DayOfWeek>,
+        startHour: Int,
+        startMinute: Int,
+        endHour: Int,
+        endMinute: Int
+    ): Course {
+        return Course(
+            name,
+            days,
+            LocalTime.of(startHour, startMinute),
+            LocalTime.of(endHour, endMinute)
+        )
+    }
+
+
+    fun addItem(course: Course): Unit {
+        items.add(0, course)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addcourses)
         val listView = findViewById<ListView>(R.id.classList)
-        val items = listOf(
-            Course("MA 261", listOf("M", "W", "F"), LocalTime.of(3, 30), LocalTime.of(4, 20)),
-            )
         val adapter = CourseListAdapter(this, items)
         listView.adapter = adapter
 
